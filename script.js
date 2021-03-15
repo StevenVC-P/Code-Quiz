ebd63f05ad44eb4f63baa695a1e911a688993b72
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-03-15 10:06:24 -0500

    added function to consolidate setting up order list for every question, adjusted formatting, working on trying to end the timer through Return and clearInterval

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button  - done
+// THEN a timer starts and I am presented with a question  -done
+// WHEN I answer a question  - done
+// THEN I am presented with another question -done
+// WHEN I answer a question incorrectly  - done
+// THEN time is subtracted from the clock - done
+// WHEN all questions are answered or the timer reaches 0  - done
+// THEN the game is over  - done
+// WHEN the game is over  - done
+// THEN I can save my initials and my score
+
 //Whats going on here?
 //quiz needs a landing page
 //a button to start the quiz
@@ -49,18 +61,19 @@ var a4 = document.createElement("button");
 var scoreBox = document.createElement("div");
 var scoreTitle = document.createElement("h3");
 var scoreNumber = document.createElement("p");
+// var timer = document.createElement("h4");
 
 //creating accessable variables for tags in HTML
 var startS1 = document.querySelector("#start");
-var timeS1 = document.querySelector(".timer");
+var timeS1 = document.querySelector("#timer");
 var mainS1 = document.getElementById("main");
 var headS1 = document.getElementById("head");
 var textS1 = document.getElementById("text");
 
 //variables for used to track score, timer, and state of page
-var mode = "startJS"
-var secondsLeft = 10
-var score = 0
+var mode = "startJS";
+var secondsLeft;
+var score;
 
 //creating object to hold all question information
 const questions = {
@@ -129,9 +142,11 @@ function addScore(){
     console.log(score);
 }
 
-//Creates question content and fills in Changes content to Question One
-function askQuestions(){
-    textS1.textContent = questions.content1.question;
+function decreaseTimer(){
+    secondsLeft = secondsLeft - 15;
+}
+
+function orderListButtons(){
     text.appendChild(orderedAnswers);
     orderedAnswers.appendChild(li1);
     orderedAnswers.appendChild(li2);
@@ -141,85 +156,118 @@ function askQuestions(){
     li2.appendChild(a2);
     li3.appendChild(a3);
     li4.appendChild(a4);
+
+}
+//Creates question content and fills in Changes content to Question One
+function askQuestions(){
+    score = 0;
+    headS1.textContent = "Welcome to Dat Code Quiz!";
+    textS1.textContent = questions.content1.question;
+
+    orderListButtons();
+
     a1.textContent = questions.content1.a;
     a2.textContent = questions.content1.b;
     a3.textContent = questions.content1.c;
     a4.textContent = questions.content1.d;
 
     a3.addEventListener("click", addScore);
+    a1.addEventListener("click", decreaseTimer);
+    a2.addEventListener("click", decreaseTimer);
+    a4.addEventListener("click", decreaseTimer);
     a1.addEventListener("click", questionTwo);
     a2.addEventListener("click", questionTwo);
     a3.addEventListener("click", questionTwo);
     a4.addEventListener("click", questionTwo);
+
 };
 
 //Changes content to Question Two
 function questionTwo(){
     textS1.textContent = questions.content2.question;
-    text.appendChild(orderedAnswers);
-    orderedAnswers.appendChild(li1);
-    orderedAnswers.appendChild(li2);
-    orderedAnswers.appendChild(li3);
-    orderedAnswers.appendChild(li4);
-    li1.appendChild(a1);
-    li2.appendChild(a2);
-    li3.appendChild(a3);
-    li4.appendChild(a4);
+
+    orderListButtons();
+
     a1.textContent = questions.content2.a;
     a2.textContent = questions.content2.b;
     a3.textContent = questions.content2.c;
     a4.textContent = questions.content2.d;
 
+    //removes previousEvent Listener
+    a3.removeEventListener("click", addScore);
+    a1.removeEventListener("click", decreaseTimer);
+    a2.removeEventListener("click", decreaseTimer);
+    a4.removeEventListener("click", decreaseTimer);
+    a1.removeEventListener("click", questionTwo);
+    a2.removeEventListener("click", questionTwo);
+    a3.removeEventListener("click", questionTwo);
+    a4.removeEventListener("click", questionTwo);
+
+    //sets new EventListener
     a4.addEventListener("click", addScore);
+    a1.addEventListener("click", decreaseTimer);
+    a2.addEventListener("click", decreaseTimer);
+    a3.addEventListener("click", decreaseTimer);
     a1.addEventListener("click", questionThree);
     a2.addEventListener("click", questionThree);
     a3.addEventListener("click", questionThree);
     a4.addEventListener("click", questionThree);
-
 };
 
 //Changes content to Question Three
 function questionThree(){
     textS1.textContent = questions.content3.question;
-    text.appendChild(orderedAnswers);
-    orderedAnswers.appendChild(li1);
-    orderedAnswers.appendChild(li2);
-    orderedAnswers.appendChild(li3);
-    orderedAnswers.appendChild(li4);
-    li1.appendChild(a1);
-    li2.appendChild(a2);
-    li3.appendChild(a3);
-    li4.appendChild(a4);
+
+    orderListButtons();
+
     a1.textContent = questions.content3.a;
     a2.textContent = questions.content3.b;
     a3.textContent = questions.content3.c;
     a4.textContent = questions.content3.d;
 
+    a4.removeEventListener("click", addScore);
+    a1.removeEventListener("click", decreaseTimer);
+    a2.removeEventListener("click", decreaseTimer);
+    a3.removeEventListener("click", decreaseTimer);
+    a1.removeEventListener("click", questionThree);
+    a2.removeEventListener("click", questionThree);
+    a3.removeEventListener("click", questionThree);
+    a4.removeEventListener("click", questionThree);
+
     a1.addEventListener("click", addScore);
+    a2.addEventListener("click", decreaseTimer);
+    a3.addEventListener("click", decreaseTimer);
+    a4.addEventListener("click", decreaseTimer);
     a1.addEventListener("click", questionFour);
     a2.addEventListener("click", questionFour);
     a3.addEventListener("click", questionFour);
     a4.addEventListener("click", questionFour);
-
 };
 
+//Changes content to Question Four
 function questionFour(){
     textS1.textContent = questions.content4.question;
+
+    orderListButtons();
+
     a1.textContent = questions.content4.a;
     a2.textContent = questions.content4.b;
     a3.textContent = questions.content4.c;
     a4.textContent = questions.content4.d;
-    text.appendChild(orderedAnswers);
-    orderedAnswers.appendChild(li1);
-    orderedAnswers.appendChild(li2);
-    orderedAnswers.appendChild(li3);
-    orderedAnswers.appendChild(li4);
-    li1.appendChild(a1);
-    li2.appendChild(a2);
-    li3.appendChild(a3);
-    li4.appendChild(a4);
+
+    a1.removeEventListener("click", addScore);
+    a2.removeEventListener("click", decreaseTimer);
+    a3.removeEventListener("click", decreaseTimer);
+    a4.removeEventListener("click", decreaseTimer);
+    a1.removeEventListener("click", questionFour);
+    a2.removeEventListener("click", questionFour);
+    a3.removeEventListener("click", questionFour);
+    a4.removeEventListener("click", questionFour);
 
     a2.addEventListener("click", addScore);
+    a1.addEventListener("click", decreaseTimer);
+    a3.addEventListener("click", decreaseTimer);
+    a4.addEventListener("click", decreaseTimer);
     a1.addEventListener("click", questionFive);
     a2.addEventListener("click", questionFive);
     a3.addEventListener("click", questionFive);
@@ -227,63 +275,79 @@ function questionFour(){
 
 };
 
+//Changes content to Question Five
 function questionFive(){
     textS1.textContent = questions.content5.question;
+
+    orderListButtons();
+
     a1.textContent = questions.content5.a;
     a2.textContent = questions.content5.b;
     a3.textContent = questions.content5.c;
     a4.textContent = questions.content5.d;
-    text.appendChild(orderedAnswers);
-    orderedAnswers.appendChild(li1);
-    orderedAnswers.appendChild(li2);
-    orderedAnswers.appendChild(li3);
-    orderedAnswers.appendChild(li4);
-    li1.appendChild(a1);
-    li2.appendChild(a2);
-    li3.appendChild(a3);
-    li4.appendChild(a4);
+
+    a2.removeEventListener("click", addScore);
+    a1.removeEventListener("click", decreaseTimer);
+    a3.removeEventListener("click", decreaseTimer);
+    a4.removeEventListener("click", decreaseTimer);
+    a1.removeEventListener("click", questionFive);
+    a2.removeEventListener("click", questionFive);
+    a3.removeEventListener("click", questionFive);
+    a4.removeEventListener("click", questionFive);
 
     a3.addEventListener("click", addScore);
+    a1.addEventListener("click", decreaseTimer);
+    a2.addEventListener("click", decreaseTimer);
+    a4.addEventListener("click", decreaseTimer);
     a1.addEventListener("click", endGame);
     a2.addEventListener("click", endGame);
     a3.addEventListener("click", endGame);
     a4.addEventListener("click", endGame);
-
 };
-
 
 //function for timer 
 function startTimer(){
     start.style.visibility = "hidden";
-    secondsLeft = 10
+    secondsLeft = 90
     var timerInterval = setInterval(function() {
         secondsLeft--;                                              //decreases seconds left DISPLAY by 1
+        timeS1.style="text-align:center;";
         timeS1.textContent = secondsLeft + " seconds remains.";     //displays the seconds left and text seconds remaining.
 
-        if(secondsLeft ===0) {                                      //if statement that watches for secondsLeft to hit zero
+        if(secondsLeft == 0) {                                       //if statement that watches for secondsLeft to hit zero
             clearInterval(timerInterval);                           //ends the countdown procedure
             endGame();                                              //moves screen to end
-
+            return
         }
     }, 1000);
+    return
 }
- 
 
 //function to create gave over screen
-function endGame(){                                                  //end screen
-    headS1.textContent = "Game Over"
+function endGame(timerInterval){                                     //end screen
+
+    a3.removeEventListener("click", addScore);
+    a1.removeEventListener("click", decreaseTimer);
+    a2.removeEventListener("click", decreaseTimer);
+    a4.removeEventListener("click", decreaseTimer);
+    a1.removeEventListener("click", endGame);
+    a2.removeEventListener("click", endGame);
+    a3.removeEventListener("click", endGame);
+    a4.removeEventListener("click", endGame);
+
+    console.log(secondsLeft);
+    timerInterval = 1;
+    timeS1.style.visibility = "hidden";
+    headS1.textContent = "Game Over";
     textS1.textContent = "Here's your score"
     start.textContent = "Restart Quiz?";
     start.style.visibility = "visible";
+    return
 }
 
 start.addEventListener("click", askQuestions);
 start.addEventListener("click", startTimer);
 start.addEventListener("click", scoreTracker);
-// var timer
-// var 
-// var
-// var 
 
 
 
